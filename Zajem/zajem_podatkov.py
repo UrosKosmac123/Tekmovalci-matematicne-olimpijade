15691f09128eb8065615de5755bd109af387ee80
Uroš Kosmač <uros.kosmac@icloud.com>
2022-01-29 23:26:30 +0100

    Popravil napake in polepšal kodo, dodal spole in države v ter vse dodal v CSV datoteke

--- Zajem/zajem_podatkov.py
+++ Zajem/zajem_podatkov.py
@@ -1,6 +1,8 @@
+import bs4
 from bs4 import BeautifulSoup
 import requests
 import csv
+import pandas
 
 seznam_drzav = requests.get('https://www.imo-official.org/countries.aspx').text
 soup_drzave = BeautifulSoup(seznam_drzav, 'lxml')
@@ -16,44 +18,81 @@ poisci_koncnice()
 #print(seznam_koncnic)
 #print(len(seznam_koncnic))
 
+def poisci_drzave():
+    text = requests.get('https://www.imo-official.org/countries.aspx').text
+    soup = bs4.BeautifulSoup(text, 'lxml')
+    return [c.text for c in soup.select('#main tbody tr td:first-child')]
 
-for koncnica in seznam_koncnic:
-    
-    seznam_podatkov = []
+drzave = poisci_drzave()
 
-    def zapisi_podatke_tekmovalcev():
-        tekmovalci = requests.get(f'https://www.imo-official.org/country_individual_r.aspx?code={koncnica}&column=year&order=desc&gender=hide').text
-        soup_tekmovalci = BeautifulSoup(tekmovalci, 'lxml')
-        podatki = soup_tekmovalci.find_all('tr')
-        for podatek in podatki:
-            seznam_podatkov.append(podatek.get_text(separator=' '))
-        
+def poisci_tekmovalce():
+    ctn = []
+    col = ['leto', 'ime', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'empty', 'skupaj', 'absolutno_mesto', 'relativno_mesto', 'nagrada']
 
-    zapisi_podatke_tekmovalcev()
+    def varen_int(val, default=None):
+        try:
+            return int(val)
+        except ValueError:
+            return default
 
-    nov_seznam_podatkov = []
-    for oseba in seznam_podatkov[2:]:
-        podatki = oseba.split(' ')
-        nov_seznam_podatkov.append(podatki)
+    def varen_float(val, default=None):
+        try:
+            return float(val)
+        except ValueError:
+            return default
 
-    seznam_let_od_1990_2020 = [] 
+    def poberi_tekmovalce_row(row, drzava):
+        tekmovalec = {'drzava': drzava }
 
-    for podatek in nov_seznam_podatkov:
-        if podatek[0] > '1989':
-            seznam_let_od_1990_2020.append(podatek)
+        for i, cell in enumerate(row.findChildren('td', recursive=False)):
+            t = cell.get_text(separator=' ')
+            if i < len(col):
+                tekmovalec[col[i]] = t
+
+        is_male = tekmovalec["ime"].find("♂") > -1
+        is_female = tekmovalec["ime"].find("♀") > -1
+            
+        tekmovalec["ime"] = tekmovalec["ime"].strip("♂♀ ")
+        if is_male:
+            tekmovalec["spol"] = "moški"
+        elif is_female:
+            tekmovalec["spol"] = "ženska"
         else:
-            pass
+            tekmovalec["spol"] = None
 
+        tekmovalec["N1"] = varen_int(tekmovalec["N1"])
+        tekmovalec["N2"] = varen_int(tekmovalec["N2"])
+        tekmovalec["N3"] = varen_int(tekmovalec["N3"])
+        tekmovalec["N4"] = varen_int(tekmovalec["N4"])
+        tekmovalec["N5"] = varen_int(tekmovalec["N5"])
+        tekmovalec["N6"] = varen_int(tekmovalec["N6"])
+        tekmovalec["skupaj"] = varen_int(tekmovalec["skupaj"])
+        tekmovalec["absolutno_mesto"] = varen_int(tekmovalec["absolutno_mesto"])
+        tekmovalec["relativno_mesto"] = varen_float(tekmovalec["relativno_mesto"].strip("%"))
+        tekmovalec.pop("empty")
+
+        return tekmovalec
+
+    for drzava in drzave:
+        text = requests.get(f'https://www.imo-official.org/country_individual_r.aspx?code={drzava}&gender=show&nameform=western').text
+        soup = bs4.BeautifulSoup(text, 'lxml')
+        ctn += [poberi_tekmovalce_row(c, drzava) for c in soup.select('#main tbody tr')]
+
+    with open('Zajem/podatki.csv', 'w', newline='') as file:
+        podatki = ['drzava', 'leto', 'ime', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'empty', 'skupaj', 'absolutno_mesto', 'relativno_mesto', 'nagrada','spol']
+        writer = csv.DictWriter(file, fieldnames=podatki)
+        writer.writerows(ctn)
+
+    #print(ctn)
+    return ctn
+
+tekmovalci = pandas.DataFrame(poisci_tekmovalce())
     #print(seznam_let_od_1990_2020)
 
-    #for koncnica in seznam_koncnic:
-    #    tekmovalci = requests.get(f'https://www.imo-official.org/country_individual_r.aspx?code={koncnica}&column=year&order=desc&gender=hide').text
-    #    soup_tekmovalci = BeautifulSoup(tekmovalci, 'lxml')
-    #    print(soup_tekmovalci.prettify())
 
-    with open(f'Drzave/{koncnica}.csv', 'w', newline='') as file:
-        zapisi = csv.writer(file, delimiter= ',')
-        zapisi.writerows(seznam_let_od_1990_2020)
+    #with open(f'Drzave/{koncnica}.csv', 'w', newline='') as file:
+    #    zapisi = csv.writer(file, delimiter= ',')
+    #    zapisi.writerows(seznam_let_od_1990_2020)
 
         
     #with open('Francija.csv', 'w', newline='') as file:
